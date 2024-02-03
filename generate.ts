ff9b01112a18a6b4dca675ac76dc1b5b79e6d08a
panthyy <fahad.alhatshami@hotmail.com>
2024-02-03 10:52:44 +0100

    updated readme

--- generate.ts
+++ generate.ts
@@ -67,6 +67,13 @@ let readme = `# PocketBase Templates
 Welcome to the PocketBase Templates repository! <br/>
 This is a community-driven collection of templates tailored for PocketBase.
 
+## Usage
+
+1. Browse the templates in the repository.
+2. Select a template that suits your project's needs and copy the contents.
+3. Go to your PocketBase Admin settings go to "Import collections" under "Sync" and paste in the contents.
+
+
 # Templates
 `;
 
@@ -114,6 +121,21 @@ for (let index = 0; index < files.length; index++) {
     `templates/${name}/schema.svg`,
     fs.readFileSync(filePaths[index], "utf-8")
   );
-
-  fs.writeFileSync("README.md", readme);
 }
+
+readme += `\n
+## Contribution
+We welcome contributions from the community! If you have a useful PocketBase template to share, please follow these steps:
+
+1. Fork this repository.
+2. Add your template with clear documentation.
+3. Submit a pull request.
+
+## Support
+
+If you have any questions, encounter issues, or want to discuss ideas related to PocketBase Templates, feel free to open an issue or join our community discussions.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE), so you're free to use, modify, and distribute these templates as needed.`;
+fs.writeFileSync("README.md", readme);
